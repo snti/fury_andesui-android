749d85ddf258619b08a2ffdd8f6302b94a3445f5
lautfernande <lautaro.fernandez@mercadolibre.com>
2020-03-10 11:43:55 -0300

    adding new components

--- components/src/main/java/com/mercadolibre/android/andesui/textfield/AndesTextfield.kt
+++ components/src/main/java/com/mercadolibre/android/andesui/textfield/AndesTextfield.kt
@@ -3,12 +3,14 @@ package com.mercadolibre.android.andesui.textfield
 import android.content.Context
 import android.support.constraint.ConstraintLayout
 import android.util.AttributeSet
+import android.view.LayoutInflater
+import com.mercadolibre.android.andesui.R
 
 class AndesTextfield : ConstraintLayout {
 
     @Suppress("unused")
     private constructor(context: Context) : super(context) {
-        throw IllegalStateException("Constructor without parameters in Andes Message is not allowed. You must provide some attributes.")
+        throw IllegalStateException("Constructor without parameters in Andes Tex is not allowed. You must provide some attributes.")
     }
 
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
@@ -24,6 +26,17 @@ class AndesTextfield : ConstraintLayout {
         andesTextfiledParser = AndesTextfieldParser.parse(context, attrs)
         val config = AndesTextfieldConfigurationFactory.create(context, andesMessageAttrs)
          */
+        initComponents()
+    }
+
+    /**
+     * Creates all the views that are part of this texfield.
+     * After a view is created then a view id is added to it.
+     *
+     */
+    private fun initComponents() {
+        val container = LayoutInflater.from(context).inflate(R.layout.andes_layout_texfield, this, true)
+
     }
 
 }
