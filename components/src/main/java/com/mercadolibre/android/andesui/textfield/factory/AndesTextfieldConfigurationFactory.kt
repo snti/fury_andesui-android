5db2c42dc2fc356987ab3c50a51a816caecefc30
lautfernande <lautaro.fernandez@mercadolibre.com>
2020-03-13 13:50:52 -0300

    icon error initial

--- components/src/main/java/com/mercadolibre/android/andesui/textfield/factory/AndesTextfieldConfigurationFactory.kt
+++ components/src/main/java/com/mercadolibre/android/andesui/textfield/factory/AndesTextfieldConfigurationFactory.kt
@@ -12,8 +12,10 @@ internal data class AndesTextfieldConfiguration(
        // val helperTextColor : ColorStateList,
         //val labelTextColor : ColorStateList,
         //val counterTextColor : ColorStateList,
-        val typeface: Typeface
-)
+        val typeface: Typeface,
+        val icon: Drawable?
+
+        )
 
 internal object AndesTextfieldConfigurationFactory {
 
@@ -27,7 +29,8 @@ internal object AndesTextfieldConfigurationFactory {
                     //helperTextColor = resolveHelperTextColor(context),
                     //labelTextColor = resolveLabelTextColor(context),
                     //counterTextColor = resolveCounterTextColor(context),
-                    typeface = resolveTypeface(context)
+                    typeface = resolveTypeface(context),
+                    icon = resolveIcon(context, state)
 
             )
         }
@@ -38,6 +41,7 @@ internal object AndesTextfieldConfigurationFactory {
     //private fun resolveLabelTextColor(context: Context) : ColorStateList
     //private fun resolveCounterTextColor(context: Context) : ColorStateList
     private fun resolveTypeface(context: Context) =  context.getFontOrDefault(R.font.andes_font_regular)
+    private fun resolveIcon(context: Context, state: AndesTextfieldStateInterface) : Drawable? = state.icon(context)
 
 
 }
--- components/src/main/java/com/mercadolibre/android/andesui/textfield/state/AndesTextfieldStateInterface.kt
+++ components/src/main/java/com/mercadolibre/android/andesui/textfield/state/AndesTextfieldStateInterface.kt
@@ -1,11 +1,15 @@
 package com.mercadolibre.android.andesui.textfield.state
 
 import android.content.Context
+import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.graphics.drawable.GradientDrawable
 import android.graphics.drawable.StateListDrawable
 import android.support.v4.content.ContextCompat
 import com.mercadolibre.android.andesui.R
+import com.mercadolibre.android.andesui.color.toAndesColor
+import com.mercadolibre.android.andesui.icons.OfflineIconProvider
+import com.mercadolibre.android.andesui.utils.buildColoredCircularShapeWithIconDrawable
 
 /**
  * Defines all style related properties that an [AndesTexfield] needs to be drawn properly.
@@ -14,6 +18,7 @@ import com.mercadolibre.android.andesui.R
  */
 internal sealed class AndesTextfieldStateInterface {
     abstract fun backgroundColor(context: Context): Drawable
+    abstract fun icon(context: Context): Drawable?
 
 }
 
@@ -27,6 +32,9 @@ internal object AndesEnabledTexfieldState : AndesTextfieldStateInterface() {
         }
     }
 
+    override fun icon(context: Context): Drawable? = null
+
+
 }
 
 
@@ -38,6 +46,16 @@ internal object AndesErrorTexfieldState : AndesTextfieldStateInterface() {
         }
     }
 
+    override fun icon(context: Context): Drawable? {
+        return buildColoredCircularShapeWithIconDrawable(
+                OfflineIconProvider(context).loadIcon("andes_ui_feedback_warning_24") as BitmapDrawable,
+                context,
+                R.color.andes_white.toAndesColor(),
+                R.color.andes_red_500,
+                context.resources.getDimension(R.dimen.andes_textfield_icon_diameter).toInt())
+    }
+
+
 }
 
 
@@ -45,6 +63,8 @@ internal object AndesDisabledTexfieldState : AndesTextfieldStateInterface() {
     override fun backgroundColor(context: Context): Drawable {
         return createGradientDrawableWithDash(context, context.resources.getDimension(R.dimen.andes_textfield_simple_stroke).toInt(), ContextCompat.getColor(context, R.color.andes_gray_200), context.resources.getDimension(R.dimen.andes_textfield_dash))
     }
+
+    override fun icon(context: Context): Drawable? = null
 }
 
 
