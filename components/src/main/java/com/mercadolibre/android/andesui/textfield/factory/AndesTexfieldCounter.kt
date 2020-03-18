a94c2a92d90ef3a1fbed9d2ad3081f63ac96b314
Guillermo Tchantouria <guillermo.tchantouria@gmail.com>
2020-03-18 10:07:21 -0300

    Textfield counter with 0 as default

--- components/src/main/java/com/mercadolibre/android/andesui/textfield/factory/AndesTexfieldCounter.kt
+++ components/src/main/java/com/mercadolibre/android/andesui/textfield/factory/AndesTexfieldCounter.kt
@@ -1,4 +1,4 @@
 package com.mercadolibre.android.andesui.textfield.factory
 
-data class AndesTextfieldCounter(val minLength: Int? = null,
-                                     val maxLength: Int? = null)
+data class AndesTextfieldCounter(val minLength: Int = 0,
+                                     val maxLength: Int = 0)
