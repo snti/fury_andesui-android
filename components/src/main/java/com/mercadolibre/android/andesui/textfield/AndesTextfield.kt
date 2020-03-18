432923a6bd068e3e2855d2798e236747ff9b6d57
Guillermo Tchantouria <guillermo.tchantouria@gmail.com>
2020-03-18 12:49:15 -0300

    Counter completed

--- components/src/main/java/com/mercadolibre/android/andesui/textfield/AndesTextfield.kt
+++ components/src/main/java/com/mercadolibre/android/andesui/textfield/AndesTextfield.kt
@@ -222,6 +222,18 @@ class AndesTextfield : ConstraintLayout {
 
             textComponent.filters = arrayOf<InputFilter>(InputFilter.LengthFilter(config.counterMaxLength!!))
         }
+
+        textComponent.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                counterComponent.text = "${count}/${config.counterMaxLength}"
+            }
+        })
     }
 
     /**
