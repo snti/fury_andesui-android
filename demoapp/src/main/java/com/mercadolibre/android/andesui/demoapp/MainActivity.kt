eee45756c7b51f4984fef696a7705b1025343d77
Martín Pérez <jose.martin@mercadolibre.com>
2020-06-18 17:46:33 -0300

    refactor demoapp

--- demoapp/src/main/java/com/mercadolibre/android/andesui/demoapp/MainActivity.kt
+++ demoapp/src/main/java/com/mercadolibre/android/andesui/demoapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import com.mercadolibre.android.andesui.button.AndesButton
 import kotlinx.android.synthetic.main.andesui_demoapp_main.*
 
 /**
