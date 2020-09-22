8535deff53a221c863b9dae6b588769fd71184cc
Federico Cano <federico.cano@mercadolibre.com>
2020-09-22 16:02:29 -0300

    added bottomSheet title and button to attrs

--- components/src/main/java/com/mercadolibre/android/andesui/bottomsheet/factory/AndesBottomSheetAttrsParser.kt
+++ components/src/main/java/com/mercadolibre/android/andesui/bottomsheet/factory/AndesBottomSheetAttrsParser.kt
@@ -1,10 +1,10 @@
 package com.mercadolibre.android.andesui.bottomsheet.factory
 
 import android.content.Context
-import android.graphics.Color
 import android.util.AttributeSet
 import com.mercadolibre.android.andesui.R
 import com.mercadolibre.android.andesui.bottomsheet.state.AndesBottomSheetState
+import com.mercadolibre.android.andesui.bottomsheet.title.AndesBottomSheetTitleAlignment
 import com.mercadolibre.android.andesui.utils.pxToDp
 
 private const val DEFAULT_CORNER_RADIUS = 6
@@ -14,13 +14,19 @@ internal data class AndesBottomSheetAttrs(
     val andesBottomSheetPeekHeight: Int,
     val andesBottomSheetCornerRadius: Int,
     val andesBottomSheetBackgroundColor: Int,
-    val andesBottomSheetState: AndesBottomSheetState
+    val andesBottomSheetState: AndesBottomSheetState,
+    val andesBottomSheetTitleText: String?,
+    val andesBottomSheetTitleAlignment: AndesBottomSheetTitleAlignment,
+    val andesBottomSheetButtonText: String?
 )
 
 internal object AndesBottomSheetAttrsParser {
 
     private const val ANDES_BOTTOM_SHEET_STATE_COLLAPSED = "1000"
     private const val ANDES_BOTTOM_SHEET_STATE_EXPANDED = "1001"
+    private const val ANDES_BOTTOM_SHEET_TITLE_CENTER = "2000"
+    private const val ANDES_BOTTOM_SHEET_TITLE_LEFT = "2001"
+
 
     fun parse(context: Context, attr: AttributeSet?): AndesBottomSheetAttrs {
         val typedArray = context.obtainStyledAttributes(attr, R.styleable.AndesBottomSheet)
@@ -29,6 +35,12 @@ internal object AndesBottomSheetAttrsParser {
             ANDES_BOTTOM_SHEET_STATE_COLLAPSED -> AndesBottomSheetState.COLLAPSED
             ANDES_BOTTOM_SHEET_STATE_EXPANDED -> AndesBottomSheetState.EXPANDED
             else -> AndesBottomSheetState.COLLAPSED
+        }
+
+        val titleAling = when (typedArray.getString(R.styleable.AndesBottomSheet_andesBottomSheetTitleAlign)) {
+            ANDES_BOTTOM_SHEET_TITLE_CENTER -> AndesBottomSheetTitleAlignment.CENTERED
+            ANDES_BOTTOM_SHEET_TITLE_LEFT -> AndesBottomSheetTitleAlignment.LEFT_ALIGN
+            else -> AndesBottomSheetTitleAlignment.CENTERED
         }
 
         return AndesBottomSheetAttrs(
@@ -41,7 +53,10 @@ internal object AndesBottomSheetAttrsParser {
                 andesBottomSheetBackgroundColor =
                     typedArray.getColor(R.styleable.AndesBottomSheet_andesBottomSheetBackgroundColor,
                             context.resources.getColor(R.color.andes_white)),
-                andesBottomSheetState = state
+                andesBottomSheetState = state,
+                andesBottomSheetTitleText = typedArray.getString(R.styleable.AndesBottomSheet_andesBottomSheetTitleText),
+                andesBottomSheetTitleAlignment = titleAling,
+                andesBottomSheetButtonText = typedArray.getString(R.styleable.AndesBottomSheet_andesBottomSheetButtonText)
         ).also { typedArray.recycle() }
     }
 }
--- components/src/main/java/com/mercadolibre/android/andesui/bottomsheet/factory/AndesBottomSheetConfigurationFactory.kt
+++ components/src/main/java/com/mercadolibre/android/andesui/bottomsheet/factory/AndesBottomSheetConfigurationFactory.kt
@@ -1,12 +1,16 @@
 package com.mercadolibre.android.andesui.bottomsheet.factory
 
 import com.mercadolibre.android.andesui.bottomsheet.state.AndesBottomSheetState
+import com.mercadolibre.android.andesui.bottomsheet.title.AndesBottomSheetTitleAlignment
 
 internal data class AndesBottomSheetConfiguration(
     val peekHeight: Int,
     val cornerRadius: Int,
     val backgroundColor: Int,
-    val state: AndesBottomSheetState
+    val state: AndesBottomSheetState,
+    val titleText: String?,
+    val titleAlignment: AndesBottomSheetTitleAlignment,
+    val buttonText: String?
 )
 
 internal object AndesBottomSheetConfigurationFactory {
@@ -16,7 +20,10 @@ internal object AndesBottomSheetConfigurationFactory {
                 peekHeight = andesBottomSheetAttrs.andesBottomSheetPeekHeight,
                 cornerRadius = andesBottomSheetAttrs.andesBottomSheetCornerRadius,
                 backgroundColor = andesBottomSheetAttrs.andesBottomSheetBackgroundColor,
-                state = andesBottomSheetAttrs.andesBottomSheetState
+                state = andesBottomSheetAttrs.andesBottomSheetState,
+                titleText = andesBottomSheetAttrs.andesBottomSheetTitleText,
+                titleAlignment = andesBottomSheetAttrs.andesBottomSheetTitleAlignment,
+                buttonText = andesBottomSheetAttrs.andesBottomSheetButtonText
         )
     }
 }
