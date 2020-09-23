b11c32da795ab949db1449b17dd9296d04814147
Federico Cano <federico.cano@mercadolibre.com>
2020-09-23 12:38:14 -0300

    added cases to showcase activity

--- demoapp/src/main/java/com/mercadolibre/android/andesui/demoapp/feature/andesbottomsheet/BottomSheetShowcaseActivity.kt
+++ demoapp/src/main/java/com/mercadolibre/android/andesui/demoapp/feature/andesbottomsheet/BottomSheetShowcaseActivity.kt
@@ -9,10 +9,15 @@ import android.widget.Toast
 import com.mercadolibre.android.andesui.bottomsheet.AndesBottomSheet
 import com.mercadolibre.android.andesui.bottomsheet.button.BottomSheetButtonListener
 import com.mercadolibre.android.andesui.bottomsheet.BottomSheetListener
+import com.mercadolibre.android.andesui.bottomsheet.title.AndesBottomSheetTitleAlignment
 import com.mercadolibre.android.andesui.demoapp.R
 
 class BottomSheetShowcaseActivity : AppCompatActivity(), BottomSheetListener, BottomSheetButtonListener {
     private lateinit var bottomSheet: AndesBottomSheet
+    private var showButton = false
+    private var showBackgroundDim = false
+    private var showTitle = false
+    private var leftAlignTitle = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,8 +25,6 @@ class BottomSheetShowcaseActivity : AppCompatActivity(), BottomSheetListener, Bo
 
         bottomSheet = findViewById(R.id.andes_bottom_sheet)
         bottomSheet.setBottomSheetListener(this)
-        bottomSheet.bottomSheetButtonText = "Toast Mother focker"
-        bottomSheet.setBottomSheetButtonListener(this)
     }
 
     override fun onCollapsed() {
@@ -50,5 +53,38 @@ class BottomSheetShowcaseActivity : AppCompatActivity(), BottomSheetListener, Bo
 
     override fun onBottomSheetButtonClicked() {
         Toast.makeText(applicationContext, "Button clicked!", Toast.LENGTH_SHORT).show()
+    }
+
+    fun onToggleButtonClicked(view: View) {
+        showButton = !showButton
+
+        if (showButton) {
+            bottomSheet.bottomSheetButtonText = "Toast Please!"
+            bottomSheet.setBottomSheetButtonListener(this)
+        } else {
+            bottomSheet.bottomSheetButtonText = null
+        }
+    }
+
+    fun onToggleBackgroundDimButtonClicked(view: View) {
+        showBackgroundDim = !showBackgroundDim
+        bottomSheet.isBackgroundDimEnabled = showBackgroundDim
+    }
+
+    fun onToggleTitleClicked(view: View) {
+        showTitle = !showTitle
+        if (showTitle) {
+            bottomSheet.bottomSheetTitleText = "Hello, I'm Title"
+        } else {
+            bottomSheet.bottomSheetTitleText = null
+        }
+    }
+    fun onToggleAlignButtonClicked(view: View) {
+        leftAlignTitle = !leftAlignTitle
+        if (leftAlignTitle) {
+            bottomSheet.bottomSheetTitleAlignment = AndesBottomSheetTitleAlignment.LEFT_ALIGN
+        } else {
+            bottomSheet.bottomSheetTitleAlignment = AndesBottomSheetTitleAlignment.CENTERED
+        }
     }
 }
