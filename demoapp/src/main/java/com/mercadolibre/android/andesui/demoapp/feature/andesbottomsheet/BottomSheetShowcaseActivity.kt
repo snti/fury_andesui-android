62d85075f2670c1062077e1605cba1f248447e69
Federico Cano <federico.cano@mercadolibre.com>
2020-09-28 15:41:09 -0300

    refactored showcase activity

--- demoapp/src/main/java/com/mercadolibre/android/andesui/demoapp/feature/andesbottomsheet/BottomSheetShowcaseActivity.kt
+++ demoapp/src/main/java/com/mercadolibre/android/andesui/demoapp/feature/andesbottomsheet/BottomSheetShowcaseActivity.kt
@@ -1,21 +1,27 @@
 package com.mercadolibre.android.andesui.demoapp.feature.andesbottomsheet
 
-import android.support.v7.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import com.mercadolibre.android.andesui.bottomsheet.AndesBottomSheet
 import com.mercadolibre.android.andesui.bottomsheet.BottomSheetListener
 import com.mercadolibre.android.andesui.bottomsheet.title.AndesBottomSheetTitleAlignment
 import com.mercadolibre.android.andesui.demoapp.R
+import kotlinx.android.synthetic.main.andesui_bottom_sheet_showcase.*
+
 
 class BottomSheetShowcaseActivity : AppCompatActivity(), BottomSheetListener {
     private lateinit var bottomSheet: AndesBottomSheet
     private var showBackgroundDim = false
     private var showTitle = false
     private var leftAlignTitle = false
+    private var testView: View? = null
+    private var testFragment = TestFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,18 +41,26 @@ class BottomSheetShowcaseActivity : AppCompatActivity(), BottomSheetListener {
 
     fun onAttachViewButtonClicked(view: View) {
         bottomSheet.removeViews()
+        if (testView == null) {
+            testView = View(applicationContext)
+            val params = ViewGroup.LayoutParams(MATCH_PARENT, FRAGMENT_HEIGHT)
+            testView?.layoutParams = params
+        }
 
-        val testView = View(applicationContext)
-        val params = ViewGroup.LayoutParams(MATCH_PARENT, 1000)
-        testView.layoutParams = params
-
-        bottomSheet.setView(testView)
+        bottomSheet.setView(testView!!)
+        bottomSheet.expand()
     }
 
     fun onAttachFragmentButtonClicked(view: View) {
-        bottomSheet.removeViews()
+        bottomSheet.setFragment(supportFragmentManager, testFragment)
+    }
 
-        bottomSheet.setFragment(supportFragmentManager, TestFragment())
+    fun onExpandButtonClicked(view: View) {
+        bottomSheet.expand()
+    }
+
+    fun onCollapseButtonClicked(view: View) {
+        bottomSheet.collapse()
     }
 
     fun onToggleBackgroundDimButtonClicked(view: View) {
@@ -69,5 +83,22 @@ class BottomSheetShowcaseActivity : AppCompatActivity(), BottomSheetListener {
         } else {
             bottomSheet.titleAlignment = AndesBottomSheetTitleAlignment.CENTERED
         }
+    }
+
+    fun onSetPeekHeightButtonClicked(view: View) {
+        if (!andes_bottom_sheet_peek_height_text_field.text.isNullOrEmpty()) {
+            bottomSheet.peekHeight = andes_bottom_sheet_peek_height_text_field.text!!.toInt()
+            closeKeyboard(view)
+            andes_bottom_sheet_peek_height_text_field.text = ""
+        }
+    }
+
+    private fun closeKeyboard(view: View) {
+        val inputManager: InputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    companion object {
+        private const val FRAGMENT_HEIGHT = 800
     }
 }
