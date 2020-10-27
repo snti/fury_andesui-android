6a99855d1f4862b53442ee0a5eeefba1b390aaa3
new name <constanzamorillo1@gmail.com>
2020-10-27 17:53:02 -0300

    testapp

--- demoapp/src/main/java/com/mercadolibre/android/andesui/demoapp/feature/CarouselShowcaseActivity.kt
+++ demoapp/src/main/java/com/mercadolibre/android/andesui/demoapp/feature/CarouselShowcaseActivity.kt
@@ -6,13 +6,12 @@ import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.view.PagerAdapter
 import android.support.v7.app.AppCompatActivity
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.Toast
+import android.widget.*
+import com.mercadolibre.android.andesui.card.AndesCard
 import com.mercadolibre.android.andesui.carousel.AndesCarousel
 import com.mercadolibre.android.andesui.carousel.margin.AndesCarouselMargin
 import com.mercadolibre.android.andesui.carousel.utils.AndesCarouselDelegate
@@ -61,20 +60,27 @@ class CarouselShowcaseActivity : AndesCarouselDelegate, AppCompatActivity() {
         }
     }
 
-    /*private fun initViewPager() {
-        viewPagerCarousel.adapter = CustomPagerAdapter(this)
-    }*/
-
     @SuppressLint("SetTextI18n")
     override fun bind(andesCarouselView: AndesCarousel, view: View, position: Int) {
         val model = getDataSet()[position]
+        val textView1 = TextView(applicationContext)
+        textView1.setTextSize(
+                TypedValue.COMPLEX_UNIT_PX,
+                applicationContext.resources.getDimension(R.dimen.title_text_size_card)
+        )
+        textView1.setTextColor(applicationContext.resources.getColor(R.color.andes_gray_800))
+        textView1.text = model.label
         if (andesCarouselView == carouselMain) {
-            view.findViewById<Button>(R.id.button).apply {
-                setBackgroundColor(model.backgroundColor)
-                text = model.label
-            }
-            view.findViewById<ImageView>(R.id.image).apply {
-                setImageResource(model.image)
+            view.findViewById<AndesCard>(R.id.cardCarousel).apply {
+                title = "Andes Card"
+                if (position != 2)
+                    setLinkAction(
+                            "Action",
+                            View.OnClickListener {
+                                Toast.makeText(context, "Action clicked!", Toast.LENGTH_SHORT).show()
+                            }
+                    )
+                cardView = textView1
             }
         }
     }
@@ -88,39 +94,11 @@ class CarouselShowcaseActivity : AndesCarouselDelegate, AppCompatActivity() {
     override fun getDataSetSize(andesCarouselView: AndesCarousel) = getDataSet().size
 
     private fun getDataSet() = listOf(
-        Model(Color.RED, "Button Nº1", R.drawable.andes_navegacion_ventas_24),
-        Model(Color.GREEN, "Button Nº2", R.drawable.andes_navegacion_carrito_idle_24),
-        Model(Color.CYAN, "Button Nº3", R.drawable.andes_navegacion_inicio_24),
-        Model(Color.RED, "Button Nº4", R.drawable.andes_navegacion_ventas_24),
-        Model(Color.GREEN, "Button Nº5", R.drawable.andes_navegacion_carrito_idle_24),
-        Model(Color.CYAN, "Button Nº6", R.drawable.andes_navegacion_inicio_24)
-    )
-}
-
-class CustomPagerAdapter(val context: Context) : PagerAdapter() {
-
-    override fun instantiateItem(container: ViewGroup, position: Int): Any {
-        val layout = LayoutInflater.from(context).inflate(R.layout.andesui_carousel_item, container, false)
-        container.addView(layout)
-        return layout
-    }
-
-    override fun destroyItem(container: ViewGroup, position: Int, view: Any) {
-        container.removeView(view as View?)
-    }
-
-    override fun getCount() = getDataSet().size
-
-    override fun isViewFromObject(p0: View, p1: Any) = p0 == p1
-
-    private fun getDataSet() = listOf(
-            Model(Color.RED, "Button Nº1", R.drawable.andes_navegacion_ventas_24),
-            Model(Color.GREEN, "Button Nº2", R.drawable.andes_navegacion_carrito_idle_24),
-            Model(Color.CYAN, "Button Nº3", R.drawable.andes_navegacion_inicio_24),
             Model(Color.RED, "Button Nº4", R.drawable.andes_navegacion_ventas_24),
-            Model(Color.GREEN, "Button Nº5", R.drawable.andes_navegacion_carrito_idle_24),
-            Model(Color.CYAN, "Button Nº6", R.drawable.andes_navegacion_inicio_24)
-    )
+            Model(Color.RED, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", R.drawable.andes_navegacion_ventas_24),
+        Model(Color.GREEN, "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", R.drawable.andes_navegacion_carrito_idle_24),
+            Model(Color.CYAN, "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.", R.drawable.andes_navegacion_inicio_24)
+            )
 }
 
 data class Model(val backgroundColor: Int, val label: String, val image: Int)
