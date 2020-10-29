c6fde2af6dc951830c521b8b6a22e10b0cefb366
new name <constanzamorillo1@gmail.com>
2020-10-29 14:08:16 -0300

    documentation in delegate

--- components/src/main/java/com/mercadolibre/android/andesui/carousel/utils/AndesCarouselDelegate.kt
+++ components/src/main/java/com/mercadolibre/android/andesui/carousel/utils/AndesCarouselDelegate.kt
@@ -8,11 +8,23 @@ import com.mercadolibre.android.andesui.carousel.AndesCarousel
  */
 interface AndesCarouselDelegate {
 
+    /**
+     * Bind view that will executes in adapter of carousel
+     */
     fun bind(andesCarouselView: AndesCarousel, view: View, position: Int)
 
+    /**
+     * Listener that will executes when user clicks the item
+     */
     fun onClickItem (andesCarouselView: AndesCarousel, position: Int)
 
+    /**
+     * Amount of items in carousel
+     */
     fun getDataSetSize(andesCarouselView: AndesCarousel): Int
 
+    /**
+     * View that will use to render item of carousel
+     */
     fun getLayoutItem(andesCarouselView: AndesCarousel): Int
 }
