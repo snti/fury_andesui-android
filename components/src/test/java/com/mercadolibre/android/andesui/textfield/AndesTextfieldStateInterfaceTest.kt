654eb7481e3d8d081d236fcd1d56d889e6afe54a
Martín Pérez <jose.martin@mercadolibre.com>
2020-12-02 14:02:09 -0300

    Fix test

--- components/src/test/java/com/mercadolibre/android/andesui/textfield/AndesTextfieldStateInterfaceTest.kt
+++ components/src/test/java/com/mercadolibre/android/andesui/textfield/AndesTextfieldStateInterfaceTest.kt
@@ -1,5 +1,6 @@
 package com.mercadolibre.android.andesui.textfield
 
+import android.graphics.drawable.BitmapDrawable
 import android.os.Build
 import com.facebook.common.logging.FLog
 import com.facebook.drawee.backends.pipeline.Fresco
@@ -9,7 +10,10 @@ import com.facebook.imagepipeline.listener.RequestLoggingListener
 import com.facebook.soloader.SoLoader
 import com.mercadolibre.android.andesui.R
 import com.mercadolibre.android.andesui.color.toAndesColor
+import com.mercadolibre.android.andesui.color.toColor
+import com.mercadolibre.android.andesui.icons.IconProvider
 import com.mercadolibre.android.andesui.textfield.state.*
+import com.mercadolibre.android.andesui.utils.buildColoredCircularShapeWithIconDrawable
 import junit.framework.Assert.assertEquals
 import junit.framework.Assert.assertNull
 import org.junit.Before
@@ -18,6 +22,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.RuntimeEnvironment
+import org.robolectric.Shadows
 import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
@@ -60,6 +65,14 @@ class AndesTextfieldStateInterfaceTest {
         assertEquals(R.color.andes_red_500.toAndesColor(), stateInterface.labelColor())
         assertEquals(R.color.andes_gray_200.toAndesColor(), stateInterface.placeholderColor())
         assertEquals(R.color.andes_red_500.toAndesColor(), stateInterface.helpersColor())
+        val actual = stateInterface.icon(context)
+        val expected = buildColoredCircularShapeWithIconDrawable(
+                IconProvider(context).loadIcon("andes_ui_feedback_warning_16") as BitmapDrawable,
+                context,
+                R.color.andes_white.toAndesColor(),
+                R.color.andes_red_500.toColor(context),
+                context.resources.getDimension(R.dimen.andes_textfield_icon_diameter).toInt())
+        assertEquals(Shadows.shadowOf(expected).createdFromResId, Shadows.shadowOf(actual).createdFromResId)
     }
 
     @Test
